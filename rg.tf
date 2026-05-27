b6a3ea62b4b64c84de57b3c8a3700d20dad5c278
Deenanath486 <deenanathyadav486@gmail.com>
2026-05-27 15:36:25 +0530

    Add new Azure resource group RG3

--- rg.tf
+++ rg.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "RG" {
   name     = "RG2"
   location = "Central India"
 }
+
+resource "azurerm_resource_group" "RG" {
+  name     = "RG3"
+  location = "Central India"
+}
